f1152c85e1bae9fee54931efc78a44cfd4d7d69e
Dane <danehague1@googlemail.com>
2024-02-04 10:29:33 +0000

    -

--- PoE.Services/obj/Debug/net6.0/PoE.Services.AssemblyInfo.cs
+++ PoE.Services/obj/Debug/net6.0/PoE.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoE.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b43b8b44fff6776e2ca149d5d9a06948b86e941")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoE.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoE.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

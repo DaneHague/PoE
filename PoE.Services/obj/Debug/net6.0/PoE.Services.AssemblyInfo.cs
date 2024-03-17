f5c412a13241e034e45393ef302edcf1bc3de948
Dane <danehague1@googlemail.com>
2024-03-17 13:47:55 +0000

    Remove obj and bin folders from repository

--- PoE.Services/obj/Debug/net6.0/PoE.Services.AssemblyInfo.cs
+++ PoE.Services/obj/Debug/net6.0/PoE.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoE.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6802d218e32626526e4f164e1da343aebc7c3082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoE.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoE.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Poe.Shared/obj/Debug/net6.0/Poe.Shared.AssemblyInfo.cs
+++ Poe.Shared/obj/Debug/net6.0/Poe.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Poe.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6802d218e32626526e4f164e1da343aebc7c3082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Poe.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Poe.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
